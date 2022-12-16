9a039fd686af5c16edf4d384fa469f21c1ae29dc
ammicco1 <ngrecchi1@gmail.com>
2022-12-16 09:01:13 +0100

    arguments parsinf

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <strings.h>
 #include <unistd.h>
 
 #include "./include/read_file.h"
@@ -12,18 +7,21 @@
 static void usage(){
    fputs("Usage: ./main [-e (encode) | -d (decode)] <data file> <key file> \n\
               [-v (verbose)] - verbose output \n\
-              [-g (generate)] [new key path] - generate new random key \n", 
+              [-g (generate)] [new key path] - generate new random key \n\
+              [-o (output file)] [output filename] - save the output in a file\n\
+              [-h (help)] - display this help\n", 
    stderr);
 }
 
 int main(int argc, char **argv){
-   int c, v = 0, i = 0, j = 0, type = -1;
-   char *s = malloc(sizeof(char) * (8 + 1));
+   int c, v = 0, i = 0, j = 0, type = -1, out = 0;
+   char *s = (char *) malloc(sizeof(char) * (8 + 1));
+   char *ofn = NULL;
    uint8_t tmp;
-   FILE *f, *k;
+   FILE *f, *k, *o;
    uint64_t *input, *key, res, d;
 
-   while((c = getopt(argc, argv, "hevgedh:")) != -1){
+   while((c = getopt(argc, argv, "vgedo::h")) != -1){
       switch(c){
          case 'v': v = 1; break; 
          case 'g': if(!argv[2]){
@@ -36,11 +34,13 @@ int main(int argc, char **argv){
             return 1;
          case 'e': type = DES_ENC; break;
          case 'd': type = DES_DEC; break;
+         case 'o': out = 1; ofn = optarg; break;
          case 'h': usage(); return 1;
+         case '?': printf("Exit.\n"); return 4;
       }
    }
 
-   if(optind == argc){
+   if(optind == argc || argc == 1){
       usage();
 
       return 1;
@@ -55,6 +55,12 @@ int main(int argc, char **argv){
    f = fopen(argv[optind], "rb");
    k = fopen(argv[optind + 1], "rb");
 
+   if(!f || !k){
+         fputs("malloc() error in function main()!\nExit.\n", stderr);
+
+         return 2;
+      }
+
    input = readbit(f, 64);
    key = readbit(k, 64);
 
@@ -67,7 +73,29 @@ int main(int argc, char **argv){
       j++;
    }
 
-   printf("output:\t\t0x%lx (text: %s -- base64: %s)\n", d, s, encode((const unsigned char *) s, 8));
+   printf("Output:\t\t0x%lx (text: %s -- base64: %s)\n", d, s, encode((const unsigned char *) s, 8));
+
+   if(out == 1){
+      if(!ofn){
+         if(type == DES_DEC){
+            fputs("No filename specified for output, default is \"out.dec\"", stderr);
+         }else{
+            fputs("No filename specified for output, default is \"out.enc\"", stderr);
+         }
+      }
+
+      o = fopen(ofn ? ofn : "out", "wb");
+
+      if(!o){
+         fputs("malloc() error in function main()!\nExit.\n", stderr);
+
+         return 2;
+      }
+
+      fputs(s, o);
+   }
+
+   free(s);
 
    return 0;
 }
