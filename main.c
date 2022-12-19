08a83433a65695f9a30467e07e604b5929101382
ammicco1 <ngrecchi1@gmail.com>
2022-12-19 16:38:05 +0100

    fix write file bug

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,14 +19,16 @@ static void usage(){                                                            
 }
 
 int main(int argc, char **argv){
-   int i = 0, j = 0, y = 0;                                                               /* variables for loops */
-   int c, type = -1, out = 0, len, v = 0;                                                 /* variables for cli args */
+   int i = 0, j = 0, x = 0;                                                               /* variables for loops */
+   int c, type = -1, out = 0, v = 0;                                                      /* variables for cli args */
 
    char *tmp_str = (char *) malloc(sizeof(char) * (8 + 1));                         /* string for print single des block output */
    char *output_str;                                                                      /* string for print all the blocks (ECB) */
    char *output_f_name = NULL;                                                            /* file name for output in a file */
    
    FILE *file, *k, *output_file;                                                          /* files for: encode/decode file, key file and output file */
+   
+   size_t len;                                                        
    
    uint8_t tmp;                                                                           /* for read single byte from des output */
    uint64_t *input, *key, d;                                                              /* for des things */
@@ -67,35 +70,49 @@ int main(int argc, char **argv){
       type = DES_ENC;
    }
 
-   file = fopen(argv[optind], "rb");                                               /* open files for data and key */
-   k = fopen(argv[optind + 1], "rb");
+   file  = fopen(argv[optind], "rb");                                               /* open files for data and key */
+   k     = fopen(argv[optind + 1], "rb");
 
    if(!file || !k){                                                                                /* check id fopen returns correctly */
-         fputs("file or key malloc() error in function main()!\nExit.\n", stderr);
+      fputs("file or key malloc() error in function main()!\nExit.\n", stderr);
 
-         return 2;
+      return 2;
    }
 
    input = read_all_file(file, 64);                            /* read all the data file and save in 64 bit chunk array (last cell is 0) */
-   key = readNbit(k, 64);                                /* read 64 bit from the key's file                          */
+   key   = readNbit(k, 64);                              /* read 64 bit from the key's file                          */
 
    if(out == 1){                                                          /* if user output in file, calculate the file length */
       fseek(file, 0, SEEK_END);
-      len = ftell(file) + 1;           
+      len = ftell(file);           
 
       output_str = (char *) malloc(8 * len);                         /* and allocate space for the output string to save in file */                       
-   } 
+   
+      if(!output_f_name){                                                                             /* if user not specify output file name, exit */
+         fputs("No filename specified for output, default!\nUse \"-o <output file name>\"\nExit.\n", stderr);
 
-   while(input[y] != 0){                                                   /* execute des ECB, encode/decode every block and concatenate all */
-      j = 0;
+         return 3;
+      }
 
-      d = des(input[y], *(key), v, type);    /* execute des on 64 bit block */
+      output_file = fopen(output_f_name, "wb");                                       /* open output file */                        
 
-      for(i = 7; i >= 0; i--){                                             /* transofrm des output in a string */
-         tmp = d >> (8 * i);
+      if(!output_file){                                                                               /* check if fopen returns correctly */
+         fputs("output_file malloc() error in function main()!\nExit.\n", stderr);
 
-         tmp_str[j] = (char) tmp;
-         j++;
+         return 2;
+      }
+   }
+
+   while(i < len / 8){                                                   /* execute des ECB, encode/decode every block and concatenate all */
+      x = 0;
+
+      d = des(input[i], *(key), v, type);    /* execute des on 64 bit block */
+
+      for(j = 7; j >= 0; j--){                                             /* transofrm des output in a string */
+         tmp = d >> (8 * j);
+
+         tmp_str[x] = (char) tmp;
+         x++;
       }
 
       printf("Output:\t\t0x%lx (text: %s -- base64: %s)\n", 
@@ -105,31 +122,16 @@ int main(int argc, char **argv){
       );
 
       if(out == 1){                                                        /* if have to save output in file, concatenate the single block string */
-         strcat(output_str, tmp_str);
+         fwrite(tmp_str, 8, 1, output_file);
       }
 
-      y++;
+      i++;
    }
 
    free(tmp_str);
+   fclose(output_file);
+   fclose(file);
+   fclose(k);
 
-   if(out == 1){                    
-      if(!output_f_name){                                                                 /* if user not specify output file name, exit */
-         fputs("No filename specified for output, default!\nUse \"-o <output file name>\"\nExit.\n", stderr);
-
-         return 3;
-      }
-
-      output_file = fopen(output_f_name, "wb");                                     /* open output file */                        
-
-      if(!output_file){                                                                               /* check if fopen returns correctly */
-         fputs("output_file malloc() error in function main()!\nExit.\n", stderr);
-
-         return 2;
-      }
-
-      fputs(output_str, output_file);                                                  /* write output file */
-   }
-
-   return 0;                                                                                     /* finally return */
+   return 0;                                                                                          /* finally return */
 }
